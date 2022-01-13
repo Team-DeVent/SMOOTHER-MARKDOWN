0b0ec64c9498cd351a27482a8333a0f06c7c8400
DipokalLab <hhj@devent.kr>
2022-01-13 16:26:34 +0900

    feat: show markdown browser
    + 마크다운 브라우저 생성
    + 소개 페이지 문구 수정

--- js/index.js
+++ js/index.js
@@ -14,3 +14,16 @@ async function loadFooter() {
     let data = await getArticle("/articles/footer.html")
     body.innerHTML = data
 }
+
+async function loadBrowser() {
+    let body = document.querySelector("#__body")
+    let data = await getArticle("/articles/browser.html")
+    body.innerHTML = data
+    loadRoom()
+}
+
+async function loadModals() {
+    let body = document.querySelector("#__modals")
+    let data = await getArticle("/articles/modal.html")
+    body.innerHTML = data
+}
--- js/storage.js
+++ js/storage.js
@@ -7,3 +7,48 @@ function loadData(itemKey) {
     let dataStorage = localStorage.getItem('data_'+itemKey);
     return dataStorage
 }
+
+function onclickCreateRoom() {
+    let title = document.querySelector("#markdown_title").value
+    createRoom({name:title})
+}
+
+function createRoom(data) {
+    let lists = [];
+    let room_list = JSON.parse(localStorage.getItem('room_list'));
+    if (room_list == null) {
+        lists.push(data)
+        localStorage.setItem('room_list', JSON.stringify(lists));
+    } else {
+        lists = room_list
+        lists.push(data)
+        localStorage.setItem('room_list', JSON.stringify(lists));
+    }
+    loadRoom() 
+}
+
+function loadRoom() {
+    let room_list = JSON.parse(localStorage.getItem('room_list'))
+    try {
+        patchRoom(room_list)
+    } catch (error) {
+        let room_list = JSON.parse(localStorage.getItem('room_list'));
+
+    }
+
+    return room_list
+}
+
+function patchRoom(lists) {
+    let body_list = document.querySelector("#md_lists")
+    body_list.innerHTML = ''
+    lists.forEach(element => {
+        body_list.insertAdjacentHTML('beforeend', `<div class="alert alert-light" onclick="locationMarkdown('${element.name}')"><b>${element.name}.md</b></div>`)
+    });
+}
+
+
+function locationMarkdown(name) {
+    location.href = window.location.href+'#'+name;
+    location.reload()
+}
